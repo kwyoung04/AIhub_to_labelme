aca0399f31314ac347d4477b65e5859d59de6780
kwyoung04 <59862512+kwyoung04@users.noreply.github.com>
2023-06-07 15:12:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,14 +13,11 @@ def check_img(jsonfile):
 
 def aihub_to_labelme(save_dir, jsonfile, check_list):
     imgname = jsonfile.replace('_BBOX.json', '.png')
-    imgname = imgname.replace('Annotations', 'Images')
+    imgname = imgname.replace('annotation', 'images')
+    #imgname = os.path.basename(imgname) if os.path.isfile(imgname) else os.path.basename(imgname.replace('.jpg', '.png'))
     
-    imgname = os.path.basename(imgname) if os.path.isfile(imgname) else os.path.basename(imgname.replace('.jpg', '.png'))
-    
-    #img = plt.imread(imgname)
-    #img_h, img_w = img.shape[:2]
-    img_h = 720
-    img_w = 1280
+    img = plt.imread(imgname)
+    img_h, img_w = img.shape[:2]
     
     with open(jsonfile) as f :
         try :
@@ -49,28 +46,35 @@ def aihub_to_labelme(save_dir, jsonfile, check_list):
                           'shape_type':'rectangle',
                           'flags': {}})
         
-    #fname = os.path.join(save_dir, os.path.basename(jsonfile))
     if not bbox_info:
         print("empty bbox")
-        return        
+        return []
     
     with open(save_dir, 'w', encoding='utf-8') as outfile:
         print("save bbox")
         data = {'version': '5.2.1', 'flags': {}, 'shapes': bbox_info, 'imagePath': imgname, 'imageData': None, 'imageHeight': img_h, 'imageWidth': img_w}
         json.dump(data, outfile, ensure_ascii=False, indent = 4)
+   
+def get_json_files(input_dir):
+    json_files = []
+    for root, dirs, files in os.walk(input_dir):
+        for file in files:
+            if file.endswith('.json'):
+                json_files.append(os.path.join(root, file))
+    return json_files
         
 if __name__ == '__main__':
-    json_file = sys.argv[1]
-    json_file = "/mnt/c/Users/Eric/Documents/src/AIhub_to_labelme/data/Validation/" + json_file
-    
-    #json_file = "data/V1F_HY_9344_20160212_020214_E_CH2_Seoul_Sun_Frontback_Day_41220_PLINE.json"
-    #json_file = "./data/" + json_file
-    #print("### json_file: ", json_file)
+    print(sys.argv)
+    json_files = get_json_files(sys.argv[1])
+    labelme_path = sys.argv[2]
+
     check_list = {8, 9, 10}
+    
+    for json_file in json_files:
+        print("# json_file: ", json_file)
+        if json_file:
+            new_path = labelme_path + json_file.split('/')[-1]
+            new_path = new_path.replace('_BBOX.json', '.json')
+            
 
-    if json_file:
-        new_path = "../labelme/" + json_file.split('/')[-1]
-        new_path = new_path.replace('_BBOX.json', '.json')
-        
-
-        aihub_to_labelme(new_path, json_file, check_list)
+            aihub_to_labelme(new_path, json_file, check_list)
